3e5b1dc7a35807969d8a6b03e3c5b8fa6f9eb048
Niako2013 <baiadze97@bk.ru>
2025-06-18 06:22:40 -0700

    commit

---
+++ day 00/day 06/classwork/classwork.py
@@ -0,0 +1,30 @@
+#1) კომენტარებით ახსენი რა არის case-sensitive და snake-case, შემდეგ ორივეზე დაწერე 3 მაგალითი, ასევე გაასწორეთ ეს კოდები:
+
+#1)
+Name = 'nika'
+print(Name)
+#2)
+goa_best = 100
+print(goa_best)
+#3)
+name='mate'
+print(name)
+#4)
+number = 10
+print(number)
+#5)
+goabest = "100%"
+print(goabest)
+
+#writing phrases without spaces where spaces are replaced by underscores and the words are usually all lowercase
+#Examples of computer-related data that is often but not always case-sensitive include commands username file names programming language tags variables and passwords
+#case-sensitive
+nAMe="nika"
+name="salome"
+NAME="giorgi"
+print(name) #ტემინალზე გამოჩნდება salome რადგან პითონი არის case-sensitive ენა
+
+Goa_best=90
+goa_Best=99
+goa_best=100
+print(goa_best)
---
+++ day 00/day 06/homeework/homework.py
@@ -0,0 +1,53 @@
+```
+1) უყურეთ ჩანაწერს თავიდან  
+
+2) მომხმარებელს შემატანინე, ასაკი, გვარი, სახელი, დაბადების თარიღი 
+
+3) ახსენით რა არის camel-case, case-sensitive, snake-scase
+
+4) რა არის debuging? ასევე გაასწორე ეს Eror ები:
+1)
+name = 'zura'
+print(Name)
+2)
+age - 13
+print(age}
+3)
+Hello world = "Hello"
+print(hello world)
+4)
+2name = 'nika'
+print(2name)
+5)
+year > 2025
+print(year)
+```
+age=13
+print(age)
+
+2name="nika"
+print(2name)
+
+name="zura"
+print(name)
+
+year=2025
+print=(year)
+
+camel-case denotes one notation for code leik (javaskript,php)
+
+
+
+writing phrases without spaces where spaces are replaced by underscores and the words are usually all lowercase
+
+name="zura"
+print(name)
+
+age=13
+print(age)
+
+2name="nika"
+print(nika)
+
+year=2025
+print(year)
